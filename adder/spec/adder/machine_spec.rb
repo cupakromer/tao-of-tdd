d9efcce37e39114ae6e24acf47e4c68db29b16f8
Aaron Kromer <aaron.kromer@gmail.com>
2013-12-17 19:22:54 -0500

    Add failing sum unit test.
    Once again it's time to write the failing test first. This type of test
    is a unit test. It's only testing a single object. The goals with unit
    tests is, given various states, verify behavior of a single object.
    
    We've also, once again, marked the test as a `wip` as it's failing; and
    we don't want to break the build. In a normal system, this commit will
    most likely not exist. Instead it will be committed along with the
    passing behavior having been implemented. It is being committed
    standalone to clearly delineate that we are writing the failing unit
    test first.
    
    Future commits may merge the behaviors together and only note in the
    commit that the test was written first.

---
+++ adder/spec/adder/machine_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'adder/machine'
+
+describe Adder::Machine do
+
+  # Normally, we will add top level `context` blocks that represent different
+  # states of the object. This adder machine has no state. So we'll go straight
+  # into behavior.
+  subject(:machine) { Adder::Machine.new }
+
+  context "summing a single number" do
+    # Here we aren't going to test every possible permutation of what can be
+    # provided to us. While that is how "strict TDD" works, it often has
+    # diminishing returns in real applications.
+    it "returns the same number as a BigDecimal", :wip do
+      expect(machine.sum(132)).to eq BigDecimal.new(132)
+    end
+  end
+
+end
+
